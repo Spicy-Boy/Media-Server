8f938058f479c94d5b9f22d9b08def1bfdb02927
Spicy-Boy <46385369+Spicy-Boy@users.noreply.github.com>
2024-05-05 03:38:52 -0400

    Initial pointer icon script (hover link to move pointer)

--- 0.Test-HTML/setPointerMenuHover.js
+++ 0.Test-HTML/setPointerMenuHover.js
@@ -1,6 +1,38 @@
-let selectionMenu = document.getElementById('selection-menu-ul');
+let selectionMenu = document.getElementById('selection-menu');
 
-let listElements = Array.from(selectionMenu.querySelectorAll('li'));
+let links = selectionMenu.querySelectorAll('a');
+
+let pointer = document.getElementById('pointer');
+
+const selectedLink = null;
+
+//e = element below
+links.forEach((link) => {
+    link.addEventListener("mouseenter", (event) => {
+        //indent link slightly on hover
+        link.style.paddingLeft = 35+"px";
+
+        //place pointer next to link
+        let linkRectangle = link.getBoundingClientRect();
+
+        pointer.style.top = linkRectangle.top+"px";
+
+        // let linkRectangle = link.getBoundingClientRect();
+        // // console.log('hi aaron',link);
+        // // pointer.style.height = link.style.height;
+        // pointer.style.height = 43+"px";
+        // let pointerWidth = pointer.clientWidth;
+        // pointer.style.left = linkRectangle.left - pointerWidth.left+"px";
+
+        // pointer.style.top
+
+    });
+   link.addEventListener("mouseleave", () => {
+    //    link.style.marginLeft = ""; 
+        link.style.paddingLeft = 0+"px";
+        // console.log('bye aaron',link); 
+    });
+});
 
 
 
