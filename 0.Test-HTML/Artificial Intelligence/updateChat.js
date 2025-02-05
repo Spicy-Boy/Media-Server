d9fba1e31fa6ca184792ba8efef2bed5c4ad305e
Spicy-Boy <46385369+Spicy-Boy@users.noreply.github.com>
2025-02-05 11:33:58 -0500

    Added thinking animation to robot query wait

--- 0.Test-HTML/Artificial Intelligence/updateChat.js
+++ 0.Test-HTML/Artificial Intelligence/updateChat.js
@@ -10,18 +10,46 @@ const textBox = document.getElementById('content');
 
 const submitButton = document.getElementById('submitQueryButton');
 submitButton.addEventListener("click", (e) => {
+    
     if (!waiting && textBox.textContent != null)
-        {
-            console.log('hi!!');
-            const newMessageDiv = document.createElement('div');
-            newMessageDiv.textContent = textBox.value;
-            newMessageDiv.classList.add("user-submission");
-            
-            messageBox.appendChild(newMessageDiv);
-            // vv ensures scrollbox snaps to new messages when submitted
-            messageBox.scrollTop = messageBox.scrollHeight;
+    {
+        // waiting = true; //lock user out of submitting queries
 
-            textBox.value = "";
-        }
+        const newMessageDiv = document.createElement('div');
+        newMessageDiv.textContent = textBox.value;
+        newMessageDiv.classList.add("user-submission");
+        
+        messageBox.appendChild(newMessageDiv);
+        // vv ensures scrollbox snaps to new messages when submitted
+        messageBox.scrollTop = messageBox.scrollHeight;
+
+        textBox.value = "";
+    }
+
+    //the robot message is the response from AI
+    newRobotMessageDiv = document.createElement("div");
+    newRobotMessageDiv.textContent = "🖥️: ...";
+    messageBox.appendChild(newRobotMessageDiv);
+
+    //vv while waiting for the server to come up with an answer to the user's query, show a loading animation!
+    intervalId = setInterval(animateRobotThinking, 600);
+    
+    //clearInterval(intervalId); //stops the animation cold
+    //intervalId = null;
 });
+
+let frameIndex = 0;
+const loadingFrames = ["🖥️: o..", "🖥️: .o.", "🖥️: ..o", "🖥️: ..."];
+
+
+function animateRobotThinking()
+{
+    console.log('new frame, ',frameIndex,newRobotMessageDiv.textContent);
+    newRobotMessageDiv.textContent = loadingFrames[frameIndex];
+    frameIndex++;
+    if (frameIndex > 3)
+    {
+        frameIndex = 0;
+    }
+}
 
