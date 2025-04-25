2a5eb215d4925b5d7fd24eafaec78ee9f7d9f06a
Spicy-Boy <46385369+Spicy-Boy@users.noreply.github.com>
2025-04-25 18:53:12 -0400

    Tweaking spacing, continue refresh functionality

--- public/js/refreshUserUploadsTable.js
+++ public/js/refreshUserUploadsTable.js
@@ -3,6 +3,16 @@
 let refreshButton = document.getElementById('refresh-uploads-button');
 
 refreshButton.addEventListener("click", (event) => {
-    
+
 });
+
+//vvv grabs a table element id, erases its contents, and re-queries the database 
+function generateNewUploadsListFromDB()
+{
+    let listContainer = document.getElementById("list"); //in our case, a table element
+
+    //first we make the header
+    let header = document.createElement("thead");
+       
+}
 
