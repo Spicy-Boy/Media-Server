f37eff1aacf1fd25af946af7aeb7d4d918fcd60b
Spicy-Boy <46385369+Spicy-Boy@users.noreply.github.com>
2025-05-07 15:14:27 -0400

    Fixed issue where I was targeting full table

--- public/js/searchUserUploadsTable.js
+++ public/js/searchUserUploadsTable.js
@@ -1,17 +1,20 @@
 // :D
 
 const searchBar = document.getElementById('search-table');
-let tableBody = document.getElementById('list') //id of table on user portal is "list"
+let tableBody = document.getElementById('listBody') //id of table on user portal is "list"
+
 
 searchBar.addEventListener("input", ()=>{
     const query = searchBar.value.toLowerCase();
     const rows = tableBody.querySelectorAll("tr"); //grab each row out of the table
 
     rows.forEach(row => {
-        const fileTitle = row.querySelector("file-name-link");
+        
+        const nameColumn = row.querySelector(".upload-list-name-column");
+        const fileTitle = nameColumn?.querySelector(".file-name-link");
 
-        if (fileTitle.includes(query)) {
-
+        if (fileTitle.textContent.includes(query)) {
+            console.log('HAHA');
         }
     });
 });
