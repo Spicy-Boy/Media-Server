328dcb12728c11daa0580a530aac2c6fd5f71f36
Spicy-Boy <46385369+Spicy-Boy@users.noreply.github.com>
2025-05-09 12:14:15 -0400

    First working search, not modular

--- public/js/refreshUserUploadsTable.js
+++ public/js/refreshUserUploadsTable.js
@@ -18,7 +18,7 @@ refreshButton.addEventListener("click", (event) => {
 async function generateNewUploadList()
 {
     let listContainer = document.getElementById("list"); //in our case, a table element
-    let tableBody = listContainer.querySelector("tbody"); //id of table on user portal is "list"
+    let tableBody = listContainer.querySelector(".tbody"); //id of table on user portal is "list"
 
     let files = await getUserFilesFromDB(pageUsername); //variable set by server in the userPortal template
 
--- public/js/searchUserUploadsTable.js
+++ public/js/searchUserUploadsTable.js
@@ -3,20 +3,78 @@
 const searchBar = document.getElementById('search-table');
 searchBar.value = "";
 searchBar.focus();
-let tableBody = document.getElementById('listBody') //id of table on user portal is "list"
+let tableBodyToSearch = document.getElementById('listBody') //id of table on user portal is "list"
+
+let searchedTableBody = tableBodyToSearch.cloneNode(true); //we work with searchedTable so as not to screw up the original tableBodyToSearch... not greatest naming scheme lol
+searchedTableBody.innerHTML = "";
+
+const originalTableHeight = searchedTableBody.offsetHeight;
 
 
 searchBar.addEventListener("input", ()=>{
-    const query = searchBar.value.toLowerCase();
-    const rows = tableBody.querySelectorAll("tr"); //grab each row out of the table
+    const queryUntouched = searchBar.value; //saved for formatting later
+    const query = queryUntouched.trim().toLowerCase();
+    searchedTableBody.innerHTML = "";
+
+    //tester vvv
+    console.log('queryUntouched:',queryUntouched,"\nquery:",query);
+
+    if (query == "")
+    {
+        searchedTableBody.replaceWith(tableBodyToSearch);
+        console.log('BLANK');
+        return;
+    }
+
+    const rows = tableBodyToSearch.querySelectorAll("tr"); //grab each row out of the table
 
     rows.forEach(row => {
         
-        const nameColumn = row.querySelector(".upload-list-name-column");
-        const fileTitle = nameColumn?.querySelector(".file-name-link");
+        const fileTitles = row.querySelectorAll(".file-name-link"); //presumably 2 per row, mobile and desktop formatted title elements
 
-        if (fileTitle.textContent.includes(query)) {
-            console.log('HAHA');
+        if (fileTitles[0].textContent.toLowerCase().includes(query)) {
+        //tester vv
+            // console.log('MATCH!', row);
+
+            const clonedRow = row.cloneNode(true);
+
+            fileTitles.forEach( title => {
+                
+                let highlightedFileTitles = clonedRow.querySelectorAll(".file-name-link");
+
+                highlightedFileTitles.forEach(highlightedTitle => {
+
+                    textToHighlight = highlightedTitle.textContent;
+
+                    //A REGULAR EXPRESSION!~
+                    const regex = new RegExp(`(${query})`, 'gi');
+                    const highlightedHTML = textToHighlight.replace(regex, '<span style="background-color: yellow;">$1</span>');
+
+                    highlightedTitle.innerHTML = highlightedHTML;
+
+                    //bad code-- breaks due to innerHTML being unreliable vv
+                    // textToHighlight =  textToHighlight.replace(queryUntouched,`<span style="background-color: yellow">${queryUntouched}</span>`);
+
+                    // // let dog = "HAHHAHA"
+                    // // textToHighlight =  textToHighlight.replace(queryUntouched,`<span style="background-color: yellow">${dog}</span>`);
+
+                    // highlightedTitle.innerHTML = textToHighlight;
+                });
+
+            });
+
+            searchedTableBody.appendChild(clonedRow);
         }
+    //tester vvv
+        // else
+        // {
+        //     console.log('NO MATCH');
+        // }
     });
+
+    // tableBodyToSearch.style.minHeight = originalTableHeight+"px"; //preserves height to prevent page jumps while searching
+    // console.log(tableBodyToSearch.style.minHeight,'+',searchedTableBody.style.minHeight);
+    tableBodyToSearch.replaceWith(searchedTableBody);
+
+
 });
