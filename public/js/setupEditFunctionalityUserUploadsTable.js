a9c15086234ee04b51d868d39e5323db80de50bf
Spicy-Boy <46385369+Spicy-Boy@users.noreply.github.com>
2025-05-15 18:20:19 -0400

    Added edit buttons and js

---
+++ public/js/setupEditFunctionalityUserUploadsTable.js
@@ -0,0 +1,45 @@
+//NOTE TO AARON:
+
+//You need to make sure only admins or the user that uploaded a file can edit its contents
+
+let editFilesButton = document.getElementById('edit-uploads');
+
+let editVisible = false;
+
+let allEditCheckboxes = document.querySelectorAll(".checkbox-file-user-portal");
+
+let allEditButtons = document.querySelectorAll(".edit-file-button-user-portal");
+
+allEditButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        let buttonId = button.dataset.fileid;
+        console.log(buttonId);
+    });
+})
+
+editFilesButton.addEventListener("click", (event)=>{
+    if (!editVisible) 
+    {
+        editVisible = true;
+
+        allEditButtons.forEach( button => {
+            button.style.display = "inline-block";
+        });
+
+        allEditCheckboxes.forEach( box => {
+            box.style.display = "inline-block";
+        });
+    }
+    else
+    {
+        editVisible = false;
+
+        allEditButtons.forEach( button => {
+            button.style.display = "none";
+        });
+
+        allEditCheckboxes.forEach( box => {
+            box.style.display = "none";
+        });
+    }
+});
